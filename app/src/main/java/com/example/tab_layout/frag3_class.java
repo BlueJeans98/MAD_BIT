f1cea03a63738d488a2d8305e377b93ee79871ed
bluejeans98 <jsk0821@kaist.ac.kr>
2022-01-03 17:19:37 +0900

    added fixed graph

--- app/src/main/java/com/example/tab_layout/frag3_class.java
+++ app/src/main/java/com/example/tab_layout/frag3_class.java
@@ -1,6 +1,76 @@
 package com.example.tab_layout;
 
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import androidx.fragment.app.Fragment;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.github.mikephil.charting.listener.OnChartGestureListener;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+
+import java.util.ArrayList;
+
 public class frag3_class extends Fragment {
+
+    private static final String TAG = "frag3_class";
+
+    private LineChart mChart;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment3, null);
+
+        mChart = (LineChart) root.findViewById(R.id.LineChart);
+
+        //mChart.setOnChartGestureListener(frag3_class.this);
+        //mChart.setOnChartValueSelectedListener(frag3_class.this);
+
+        mChart.setDragEnabled(true);
+        mChart.setScaleEnabled(false);
+
+        ArrayList<Entry> yValues = new ArrayList<>();
+
+        yValues.add(new Entry(0,60f));
+        yValues.add(new Entry(1,50f));
+        yValues.add(new Entry(2,70f));
+        yValues.add(new Entry(3,30f));
+        yValues.add(new Entry(4,50f));
+        yValues.add(new Entry(5,60f));
+        yValues.add(new Entry(6,65f));
+
+
+        LineDataSet set1 = new LineDataSet(yValues, "Data Set 1");
+
+        set1.setFillAlpha(110);
+
+        set1.setColor(Color.RED);
+        set1.setLineWidth(3f);
+        set1.setValueTextSize(10f);
+        set1.setValueTextColor(Color.GREEN);
+
+        ArrayList<ILineDataSet> dataSets = new ArrayList<>();
+        dataSets.add(set1);
+
+        LineData data = new LineData(dataSets);
+
+        mChart.setData(data);
+
+        return root;
+    }
+
+
+
 }
